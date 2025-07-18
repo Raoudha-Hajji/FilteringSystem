7bb831595a6e0ed207643809885fdac9168ad603
Raoudha-Hajji <ha.raoudha@gmail.com>
2025-07-18 08:34:21 +0100

    fix typo

--- Backend/scraper/urls.py
+++
@@ -1,8 +0,0 @@
-# scraper/urls.py
-from django.urls import path
-from .views import run_web_scraper_view, run_email_scraper_view
-
-urlpatterns = [
-    path('tuneps/', run_web_scraper_view, name='run_web_scraper_manual'),
-    path('tunisurf/', run_email_scraper_view, name='run_email_scraper_manual'),
-]
